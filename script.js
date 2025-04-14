a117f8626f675355c0f1e089951d614092eb54ae
ST3TJ <89393349+ST3TJ@users.noreply.github.com>
2025-04-15 00:06:41 +0500

    Big update. Changed shop tab, changed about us tab. Misc changes.

--- script.js
+++ script.js
@@ -26,6 +26,48 @@ const adv = [
     'adaptive fakelag system',
 ];
 
+const products = {
+    '1': {
+        'name': 'Stable',
+        'price': '$2',
+        'description': `
+    You get the basic version of the metamod.<br>
+    - Access to the basic functions of the script.<br>
+    - Limited number of user configs (5 per user).<br>
+    - Basic support through FAQ and publicly available materials<br>
+    - Updates once a month<br>
+    - Easy setup with no additional features<br>
+  `,
+    },
+    '2': {
+        'name': 'Beta',
+        'price': '$5',
+        'description': `
+    You are getting the beta version of the metamod.<br>
+    - All functions from the Basic version<br>
+    - Additional tools and advanced settings<br>
+    - Unlimited number of user configs<br>
+    - Technical support (responses within 24 hours)<br>
+    - Early access to beta testing of new features<br>
+    - Chat access for beta users<br>
+    - Distinctive signs ( clantag, shared icon )<br>
+  `,
+    },
+    '3': {
+        'name': 'Alpha',
+        'price': '$10',
+        'description': `
+    You get the alpha version of the metamod.<br>
+    - All features from the Basic and Beta versions<br>
+    - Weekly updates and bug fixes<br>
+    - Access to closed experimental features before their official release<br>
+    - Special round-the-clock technical support<br>
+    - Access to the developer's chat<br>
+    - The ability to add functions specifically at your request<br>
+  `,
+    },
+};
+
 let inAnimation = false;
 let lastIndex = -1;
 
@@ -35,23 +77,45 @@ function fadeInElement(element, delay = 10) {
     }, delay);
 }
 
-// Внизу очень много щит кода
+function downloadFile(url, filename) {
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = filename || '';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
+function handleSelector(value) {
+    const product = products[value];
+    const product_price = document.getElementById('product-price');
+    const product_description = document.getElementById('product-description');
+
+    product_price.textContent = product.price;
+    product_description.innerHTML = product.description;
+}
+
+// Внизу очень много щит кода ( сверху не меньше )
 // Но он работает
 // И это главное
 document.addEventListener('DOMContentLoaded', () => {
     const clickMeButton = document.querySelector('#click-me');
     const clickMeContainer = document.querySelector('#click-me-container');
 
-    const homeSection = document.getElementById('Home');
-    const descriptionElement = document.getElementById('description');
-    const logoElement = document.getElementById('logo');
-    const contactsElement = document.getElementById('contacts');
-    const navbarElement = document.getElementById('navbar');
+    const home_section = document.getElementById('Home');
+    const description_element = document.getElementById('description');
+    const logo_element = document.getElementById('logo');
+    const contacts_element = document.getElementById('contacts');
+    const navbar_element = document.getElementById('navbar');
     const logoText = document.querySelector('#logo p');
+
+    const select = document.getElementById('product-select');
 
     const cookie = document.getElementById('cookie');
     const cookie_accept = document.getElementById('cookie-accept');
     const cookie_decline = document.getElementById('cookie-decline');
+
+    const buy_button = document.querySelector('.buy-button');
 
     if (document.cookie.includes('cookieAccepted')) {
         cookie.style.display = 'none';
@@ -60,6 +124,8 @@ document.addEventListener('DOMContentLoaded', () => {
     setTimeout(() => {
         cookie.style.opacity = '1';
     }, 500);
+
+    handleSelector(select.value);
 
     cookie_accept.addEventListener('click', () => {
         document.cookie = 'cookieAccepted=true;';
@@ -83,14 +149,14 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function initializePage() {
-        homeSection.style.opacity = '1';
-        logoElement.style.display = 'block';
-        fadeInElement(logoElement);
+        home_section.style.opacity = '1';
+        logo_element.style.display = 'block';
+        fadeInElement(logo_element);
 
-        contactsElement.style.opacity = '1';
-        contactsElement.style.bottom = '0px';
-        navbarElement.style.display = 'flex';
-        fadeInElement(navbarElement);
+        contacts_element.style.opacity = '1';
+        contacts_element.style.bottom = '0px';
+        navbar_element.style.display = 'flex';
+        fadeInElement(navbar_element);
 
         let newIndex = Math.floor(Math.random() * adv.length);
         lastIndex = newIndex;
@@ -120,15 +186,15 @@ document.addEventListener('DOMContentLoaded', () => {
             setTimeout(() => {
                 clickMeButton.style.display = 'none';
                 clickMeContainer.style.display = 'none';
-                descriptionElement.style.display = 'block';
-                fadeInElement(descriptionElement);
+                description_element.style.display = 'block';
+                fadeInElement(description_element);
 
                 setTimeout(() => {
-                    descriptionElement.style.opacity = '0';
+                    description_element.style.opacity = '0';
                 }, 4000);
 
                 setTimeout(() => {
-                    descriptionElement.style.display = 'none';
+                    description_element.style.display = 'none';
                     initializePage();
                     if (document.cookie.includes('cookieAccepted')) {
                         document.cookie = 'metamod=true;';
@@ -152,6 +218,14 @@ document.addEventListener('DOMContentLoaded', () => {
             logoText.style.opacity = '1';
         }, 500);
     }, 3000);
+
+    select.addEventListener('change', () => {
+        handleSelector(select.value);
+    });
+
+    buy_button.addEventListener('click', () => {
+        downloadFile('loader.lua', 'loader.lua');
+    });
 
     const buttons = document.querySelectorAll('#navbar button');
     buttons.forEach(button => {
